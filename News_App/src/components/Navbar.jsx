9be3f4d4eda44ce191911cc2fe9db845939d307a
jeet <jeetsibes05@gmail.com>
2025-01-27 12:50:32 +0530

    Realworld news

---
+++ News_App/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <header className="bg-slate-600 h-20 w-auto">
+        <div className='flex flex-column-center text-white '>
+          <h1>Godzilla</h1>
+        </div>
+
+      </header>
+    </div>
+  )
+}
